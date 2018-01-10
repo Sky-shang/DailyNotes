5928511df5531994f8fd50c8fce59b2813eca407
zhao zilong <1393950533@qq.com>
2018-01-10 15:31:44 +0800

    新增文件

---
+++ DailyNotes/Log/Models/FlashLogLevel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DailyNotes.Log.Models
+{
+    /// <summary>
+    /// 日志等级
+    /// </summary>
+    public enum FlashLogLevel
+    {
+        Debug,
+        Info,
+        Error,
+        Warn,
+        Fatal
+    }
+}
---
+++ DailyNotes/Log/Models/FlashLogMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DailyNotes.Log.Models
+{
+    /// <summary>
+    /// 日志内容
+    /// </summary>
+    public class FlashLogMessage
+    {
+        public string Message { get; set; }
+        public FlashLogLevel Level { get; set; }
+        public Exception Exception { get; set; }
+
+    }
+}
